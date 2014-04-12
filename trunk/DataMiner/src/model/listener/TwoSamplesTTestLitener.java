a5f1b9b478a69afd7a69e5b21448248a3f07f3d5
vlada.borikic@gmail.com <vlada.borikic@gmail.com@1d3d26e2-5208-861d-c57a-c1614bd6f421>
2014-04-12 21:55:14 +0000

    Uradjen gui za Two Samples T test:

--- trunk/DataMiner/src/model/listener/TwoSamplesTTestLitener.java
+++ trunk/DataMiner/src/model/listener/TwoSamplesTTestLitener.java
@@ -3,22 +3,40 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package model.listener;
 
+import controller.Data;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.JPanel;
+import view.centerPanels.NewTableCentralPanel;
+import view.leftPanels.TwoSamplesChooserPnlLeft;
+import view.main.MainGUI;
 
 /**
  *
  * @author Stupi
  */
-public class TwoSamplesTTestLitener implements ActionListener{
+public class TwoSamplesTTestLitener implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent ae) {
-    
-    
+
+        TwoSamplesChooserPnlLeft panelLeft = new TwoSamplesChooserPnlLeft();
+        panelLeft.setSize(MainGUI.getInstance().getPnlLeft().getSize());
+        MainGUI.getInstance().getPnlLeft().removeAll();
+        MainGUI.getInstance().getPnlLeft().add(panelLeft);
+
+        JPanel panelCenter = Data.getInstance().getLoadedInstancesCentralPanel();
+//                panelCenter.setSize(MainGUI.getInstance().getPnlCenter().getSize());
+        MainGUI.getInstance().getPnlCenter().removeAll();
+        MainGUI.getInstance().getPnlCenter().add(panelCenter);
+
+        MainGUI.getInstance().validate();
+        MainGUI.getInstance().repaint();
+
+        panelLeft.setVisible(true);
+        panelCenter.setVisible(true);
     }
-    
+
 }
